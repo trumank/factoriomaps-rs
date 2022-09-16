cfa9fa6d9e3e35cc14a301717f0488f65e948a78
Truman Kilen <t@kilen.me>
2022-09-16 15:57:15 -0500

    WIP

---
+++ mod/control.lua
@@ -0,0 +1,199 @@
+function center(area)
+  return {(area.left_top.x + area.right_bottom.x) / 2, (area.left_top.y + area.right_bottom.y) / 2}
+end
+
+function chunk_key(chunk)
+  return chunk.x .. ',' .. chunk.y
+end
+
+function map(tbl, func, ...)
+    local new_tbl = {}
+    for k, v in pairs(tbl) do new_tbl[k] = func(v, k, ...) end
+    return new_tbl
+end
+
+function filter(tbl, func, ...)
+    local new_tbl = {}
+    local add = #tbl > 0
+    for k, v in pairs(tbl) do
+        if func(v, k, ...) then
+            if add then
+                table.insert(new_tbl, v)
+            else
+                new_tbl[k] = v
+            end
+        end
+    end
+    return new_tbl
+end
+
+function get_neighbors(chunks, chunk)
+  return filter(map({
+    {x =  0, y = -1},
+    {x =  0, y =  1},
+    {x = -1, y =  0},
+    {x =  1, y =  0},
+  }, function(offset) return chunks[chunk_key{x = chunk.x + offset.x, y = chunk.y + offset.y}] end),
+  function(neighbor) return neighbor ~= nil end)
+end
+
+function take_screenshots(player)
+  for name, surface in pairs(game.surfaces) do
+    local chunks = {}
+
+    -- initialize chunks and whether they contain player entities
+    for chunk in surface.get_chunks() do
+      -- player.print("x: " .. chunk.x .. ", y: " .. chunk.y)
+      -- player.print("area: " .. serpent.line(chunk.area))
+
+      local contains_entities = 0 < #surface.find_entities_filtered{area=chunk.area, force=player.force}
+      chunks[chunk_key(chunk)] = {
+        x = chunk.x,
+        y = chunk.y,
+        distance = contains_entities and 0 or nil,
+        contains_entities = contains_entities,
+      }
+    end
+
+    -- calculate residual distances
+    for i=1,5 do
+      for _, chunk in pairs(chunks) do
+        local min = nil
+        for _, neigh in pairs(get_neighbors(chunks, chunk)) do
+          -- print(serpent.line(min) .. ' ' .. serpent.line(neigh.distance))
+          if min == nil or (neigh.distance ~= nil and min > neigh.distance) then
+            min = neigh.distance
+          end
+        end
+        if min ~= nil and (chunk.distance == nil or chunk.distance > min) then
+          chunk.distance = min + 1
+        end
+      end
+    end
+
+    -- set flag if within distance of player entity
+    for _, chunk in pairs(chunks) do
+      if chunk.distance ~= nil and chunk.distance < 5 then
+        chunk.within_distance = true
+      else
+        chunk.within_distance = false
+      end
+    end
+
+    -- find and fill islands
+    local queue = {}
+    for key, chunk in pairs(chunks) do
+      queue[key] = chunk
+    end
+    local key
+    local edge_id = 0
+    while true do
+      local key, chunk = next(queue)
+      if key == nil then
+        break
+      end
+      if chunk.within_distance then
+        chunk.edge = false
+      else
+        -- search area originating from chunk
+        local visited = {}
+        local to_visit = { [key] = chunk }
+        local edge = false
+
+        edge_id = edge_id + 1
+
+        -- iterator neighbors until exhausted
+        while true do
+          local key, chunk = next(to_visit)
+
+          -- all neighbors found
+          if key == nil then
+            for key, chunk in pairs(visited) do
+              chunk.edge = edge
+              chunk.edge_id = edge_id
+              queue[key] = nil
+            end
+            break
+          end
+
+          local neighbors = get_neighbors(chunks, chunk)
+          -- if less than 4 neighbors then edge of map found
+          if #neighbors < 4 then
+            edge = true
+          end
+
+          for _, chunk in pairs(neighbors) do
+            local key = chunk_key(chunk)
+            if not chunk.within_distance and not visited[key] then
+              to_visit[key] = chunk
+            end
+          end
+
+          -- remove current chunk from queue and add to visited
+          visited[key] = chunk
+          to_visit[key] = nil
+        end
+
+      end
+      queue[key] = nil
+    end
+
+    -- create map tags
+    if false then
+      for _, tag in pairs(player.force.find_chart_tags(surface)) do
+        tag.destroy()
+      end
+
+      for _, chunk in pairs(chunks) do
+        local icon = 'signal-black'
+        if chunk.edge == true then
+          icon = 'signal-green'
+        elseif chunk.edge == false then
+          icon = 'signal-red'
+        end
+        -- local icon = 'signal-red'
+        -- if chunk.contains_entities then
+        --   icon = 'signal-green'
+        -- elseif chunk.within_distance then
+        --   icon = 'signal-black'
+        -- end
+        player.force.add_chart_tag(surface, {
+          position = {chunk.x * 32 + 16, chunk.y * 32 + 16},
+          -- text = key(chunk) .. ',' .. serpent.line(chunk.distance),
+          text = tostring(chunk.edge_id or ''),
+          icon = {type='virtual', name=icon}
+        })
+      end
+      -- print(serpent.block(chunks))
+    else
+      for _, chunk in pairs(chunks) do
+        if not chunk.edge then
+          game.take_screenshot({
+            surface = surface,
+            position = {chunk.x * 32 + 16, chunk.y * 32 + 16},
+            resolution = {1024, 1024},
+            zoom = 1,
+            path = name .. ',' .. chunk.x .. ',' .. chunk.y .. '.png',
+            show_entity_info = true
+          })
+        end
+      end
+    end
+
+    -- game.print(serpent.line(get_neighbors(chunks, {x = 0, y = 0})))
+  end
+end
+
+script.on_event(defines.events.on_tick, function(event)
+  game.set_wait_for_screenshots_to_finish()
+
+  local player = game.connected_players[1]
+
+  take_screenshots(player)
+
+  game.print('screenshot finished')
+
+  script.on_event(defines.events.on_tick, function(event) end)
+
+  -- exit()
+end)
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,24 @@
 #![feature(nll)]
 
-use std::convert::TryInto;
 use std::{fs, path::Path};
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::num::NonZeroU32;
-use std::thread;
-use std::sync::mpsc::channel;
+
+use rayon::prelude::*;
 
 use image::{DynamicImage, GenericImage, GenericImageView};
+use webp::*;
 
 use fast_image_resize as fr;
 
 
-const TILE_SIZE: u32 = 2048;
+//const TILE_SIZE: u32 = 2048;
+const TILE_SIZE: u32 = 1024;
 const MAX_ZOOM: i32 = 20;
+const INPUT_PATH: &str = "factorio/script-output";
+const OUTPUT_PATH: &str = "factorio/script-output";
 
-#[derive(Debug, Copy, Clone, Eq, PartialEq, PartialOrd, Ord, Hash)]
+#[derive(Debug, Clone, Eq, PartialEq, PartialOrd, Ord, Hash)]
 struct Tile {
     zoom: i32,
     x: i32,
@@ -24,7 +27,7 @@ struct Tile {
 
 impl Tile {
     /// Returns tile containing this tile
-    fn zoom_out(self) -> Tile {
+    fn zoom_out(&self) -> Tile {
         Tile {
             zoom: self.zoom - 1,
             x: self.x / 2,
@@ -32,7 +35,7 @@ impl Tile {
         }
     }
     /// Returns tile inside this tile with the smallest coordinates
-    fn zoom_in(self) -> Tile {
+    fn zoom_in(&self) -> Tile {
         Tile {
             zoom: self.zoom + 1,
             x: self.x * 2,
@@ -40,7 +43,7 @@ impl Tile {
         }
     }
     /// Returns translated Tile offset
-    fn translate(self, x: i32, y: i32) -> Tile {
+    fn translate(&self, x: i32, y: i32) -> Tile {
         Tile {
             zoom: self.zoom,
             x: self.x + x,
@@ -49,105 +52,178 @@ impl Tile {
     }
 }
 
-#[derive(Debug,Clone)]
-struct Surface {
-    name: String,
-}
-
-impl Surface {
-    fn new(name: String) -> Surface {
-        Surface {
-            name,
-        }
-    }
-    fn load(&self, tile: Tile) -> Option<DynamicImage> {
-        let img = if tile.zoom == MAX_ZOOM {
-            println!("loading tile from disk {:#?}", tile);
-            let string = &format!("factorio/script-output/{},{},{}.png", self.name, tile.x, tile.y);
-            let path = Path::new(&string);
-            image::open(&path).ok()
-        } else {
-            let zoom = tile.zoom_in();
-
-            let q1 = self.load(zoom);
-            let q2 = self.load(zoom.translate(1, 0));
-            let q3 = self.load(zoom.translate(0, 1));
-            let q4 = self.load(zoom.translate(1, 1));
-
-            if q1.is_some() || q2.is_some() || q3.is_some() || q4.is_some() {
-                let mut full_size = DynamicImage::new_rgba8(TILE_SIZE * 2, TILE_SIZE * 2);
-                if let Some(t) = q1 { full_size.copy_from(&t, 0, 0).unwrap(); }
-                if let Some(t) = q2 { full_size.copy_from(&t, TILE_SIZE, 0).unwrap(); }
-                if let Some(t) = q3 { full_size.copy_from(&t, 0, TILE_SIZE).unwrap(); }
-                if let Some(t) = q4 { full_size.copy_from(&t, TILE_SIZE, TILE_SIZE).unwrap(); }
-
-                //let resized = full_size.resize(TILE_SIZE, TILE_SIZE, image::imageops::FilterType::Nearest);
-
-                let width = NonZeroU32::new(full_size.width()).unwrap();
-                let height = NonZeroU32::new(full_size.height()).unwrap();
-                let src_image = fr::Image::from_vec_u8(
-                    width,
-                    height,
-                    full_size.into_rgba8().into_raw(),
-                    fr::PixelType::U8x4,
-                ).unwrap();
-
-                // Create container for data of destination image
-                let dst_width = NonZeroU32::new(TILE_SIZE).unwrap();
-                let dst_height = NonZeroU32::new(TILE_SIZE).unwrap();
-                let mut dst_image = fr::Image::new(
-                    dst_width,
-                    dst_height,
-                    src_image.pixel_type(),
-                );
-
-                // Get mutable view of destination image data
-                let mut dst_view = dst_image.view_mut();
-
-                // Create Resizer instance and resize source image
-                // into buffer of destination image
-                let mut resizer = fr::Resizer::new(
-                    fr::ResizeAlg::Convolution(fr::FilterType::Lanczos3),
-                );
-                resizer.resize(&src_image.view(), &mut dst_view).unwrap();
-
-                // Divide RGB channels of destination image by alpha
-                //alpha_mul_div.divide_alpha_inplace(&mut dst_view).unwrap();
-                let resized = DynamicImage::ImageRgba8(image::RgbaImage::from_raw(
-                    TILE_SIZE, TILE_SIZE,
-                    dst_image.into_vec()
-                ).unwrap());
-                Some(resized)
-            } else {
-                None
-            }
-        };
-
-        if let Some(ref img) = img {
-            let parts = 4;
-            let part_size = TILE_SIZE / parts;
-            for x in 0..parts {
-                for y in 0..parts {
-                    let sub_img = img.view(x * part_size, y * part_size, part_size, part_size).to_image();
-                    let path_str = &format!(
-                        "web/Images/4/{}/day/{}/{}/{}.png",
-                        &self.name,
-                        tile.zoom,
-                        x as i32 + tile.x * parts as i32,
-                        y as i32 + tile.y * parts as i32
-                    );
-                    let path = Path::new(path_str);
-                    fs::create_dir_all(&path.parent().unwrap()).unwrap();
-                    sub_img.save(&path).unwrap();
-                }
-            }
+fn create_tile(surface: &Surface, tile: &Tile, progress: &indicatif::ProgressBar) -> Option<DynamicImage> {
+    let img = if tile.zoom == MAX_ZOOM {
+        //println!("reading {:?}", tile);
+        //let string = &format!("factorio/script-output/{},{},{}.png", surface, tile.x, tile.y);
+        let path = Path::new(INPUT_PATH).join(&format!("{},{},{}.png", surface.name, tile.x, tile.y));
+        let img = image::open(&path).ok();
+        if img.is_some() {
+            progress.inc(1)
         }
         img
+    } else {
+        let zoom = tile.zoom_in();
+
+        let q: Vec<_> = vec![
+            (0, 0),
+            (1, 0),
+            (0, 1),
+            (1, 1),
+        ].into_par_iter()
+            .map(|(x, y)| ((x, y), create_tile(surface, &zoom.translate(x, y), progress)))
+            .filter_map(|data| match data {
+                (p, Some(img)) => Some((p, img)),
+                _ => None,
+            })
+            .collect();
+
+        if !q.is_empty() {
+            let mut full_size = DynamicImage::new_rgba8(TILE_SIZE * 2, TILE_SIZE * 2);
+            q.iter().for_each(|((x, y), img)| full_size.copy_from(img, *x as u32 * TILE_SIZE, *y as u32 * TILE_SIZE).unwrap());
+
+            //let resized = full_size.resize(TILE_SIZE, TILE_SIZE, image::imageops::FilterType::Nearest);
+
+            let width = NonZeroU32::new(full_size.width()).unwrap();
+            let height = NonZeroU32::new(full_size.height()).unwrap();
+            let src_image = fr::Image::from_vec_u8(
+                width,
+                height,
+                full_size.into_rgba8().into_raw(),
+                fr::PixelType::U8x4,
+            ).unwrap();
+
+            // Create container for data of destination image
+            let dst_width = NonZeroU32::new(TILE_SIZE).unwrap();
+            let dst_height = NonZeroU32::new(TILE_SIZE).unwrap();
+            let mut dst_image = fr::Image::new(
+                dst_width,
+                dst_height,
+                src_image.pixel_type(),
+            );
+
+            // Get mutable view of destination image data
+            let mut dst_view = dst_image.view_mut();
+
+            // Create Resizer instance and resize source image
+            // into buffer of destination image
+            let mut resizer = fr::Resizer::new(
+                fr::ResizeAlg::Convolution(fr::FilterType::Lanczos3),
+            );
+            resizer.resize(&src_image.view(), &mut dst_view).unwrap();
+
+            // Divide RGB channels of destination image by alpha
+            //alpha_mul_div.divide_alpha_inplace(&mut dst_view).unwrap();
+            let resized = DynamicImage::ImageRgba8(image::RgbaImage::from_raw(
+                TILE_SIZE, TILE_SIZE,
+                dst_image.into_vec()
+            ).unwrap());
+            Some(resized)
+        } else {
+            None
+        }
+    };
+
+    if let Some(ref img) = img {
+        let parts = 2;
+        let part_size = TILE_SIZE / parts;
+        for x in 0..parts {
+            for y in 0..parts {
+                let sub_img = img.view(x * part_size, y * part_size, part_size, part_size).to_image();
+                let path_str = &format!(
+                    "web/Images/4/{}/day/{}/{}/{}.png",
+                    &surface.name,
+                    tile.zoom,
+                    x as i32 + tile.x * parts as i32,
+                    y as i32 + tile.y * parts as i32
+                );
+                let path = Path::new(path_str);
+                fs::create_dir_all(&path.parent().unwrap()).unwrap();
+                //sub_img.save(&path).unwrap();
+
+                let dyn_img = DynamicImage::from(sub_img);
+                let encoder = Encoder::from_image(&dyn_img).unwrap();
+                //let webp = encoder.encode_lossless();
+                let webp = encoder.encode(40.0);
+                std::fs::write(Path::new(path_str).with_extension("webp"), &*webp).unwrap();
+            }
+        }
+    }
+    img
+}
+
+const fn num_bits<T>() -> usize { std::mem::size_of::<T>() * 8 }
+fn log_2(x: i32) -> u32 {
+    assert!(x > 0);
+    num_bits::<i32>() as u32 - x.leading_zeros() - 1
+}
+
+fn create_tiles(surface: &Surface) {
+    let progress = indicatif::ProgressBar::new(surface.tiles.len() as u64);
+    progress.set_style(
+        indicatif::ProgressStyle::with_template("{wide_bar} Elapsed: {elapsed_precise}, ETA: {my_eta}").unwrap()
+            .with_key("my_eta", |s: &indicatif::ProgressState, w: &mut dyn std::fmt::Write| {
+                 match (s.pos(), s.len()) {
+                    (pos,Some(len)) => write!(w, "{:#}", humantime::format_duration(std::time::Duration::from_secs(s.elapsed().as_secs() * (len-pos)/pos))),
+                    _ => write!(w, "-"),
+                }.unwrap()
+            })
+    );
+
+    let max = std::cmp::max(
+        std::cmp::max(
+            -surface.min_x,
+            -surface.min_y,
+        ),
+        std::cmp::max(
+            1 + surface.max_x,
+            1 + surface.max_y,
+        )
+    );
+    let zoom = MAX_ZOOM - log_2(max) as i32 - 1; // TODO off by one?
+    println!("{}", zoom);
+    vec![
+        Tile { zoom, x: -1, y: -1 },
+        Tile { zoom, x:  0, y: -1 },
+        Tile { zoom, x: -1, y:  0 },
+        Tile { zoom, x:  0, y:  0 },
+    ].par_iter().for_each(|t| {
+        create_tile(&surface, &t, &progress);
+    });
+}
+
+fn create_map(surface: Surface) {
+    let mut full_size = DynamicImage::new_rgba8(TILE_SIZE * (1 + surface.width() as u32), TILE_SIZE * (1 + surface.height() as u32));
+
+    for tile in surface.tiles {
+        println!("{:?}", tile);
+        let path = Path::new(INPUT_PATH).join(&format!("{},{},{}.png", surface.name, tile.x, tile.y));
+        let img = image::open(&path).unwrap();
+        full_size.copy_from(&img, (tile.x - surface.min_x) as u32 * TILE_SIZE, (tile.y - surface.min_y) as u32 * TILE_SIZE).unwrap();
+    }
+    full_size.save(Path::new("full.png")).unwrap();
+}
+
+#[derive(Debug)]
+struct Surface {
+    name: String,
+    tiles: Vec<Tile>,
+    min_x: i32,
+    max_x: i32,
+    min_y: i32,
+    max_y: i32,
+}
+impl Surface {
+    fn width(&self) -> i32 {
+        self.max_x - self.min_x
+    }
+    fn height(&self) -> i32 {
+        self.max_y - self.min_y
     }
 }
 
 fn main() {
-    let paths = fs::read_dir("factorio/script-output").unwrap();
+    let paths = fs::read_dir(INPUT_PATH).unwrap();
 
     let mut surfaces = HashMap::new();
 
@@ -155,63 +231,38 @@ fn main() {
         let path = entry.unwrap().path();
         let name = path.file_stem().and_then(std::ffi::OsStr::to_str).unwrap();
         let mut split = name.split(",");
-        let surface = split.next().unwrap().to_owned();
+        let surface_name = split.next().unwrap().to_owned();
         let x = split.next().unwrap().parse::<i32>().unwrap();
         let y = split.next().unwrap().parse::<i32>().unwrap();
-        //println!("Name: {}", path.unwrap().path().display())
 
         println!("{:#?}", Tile {
             zoom: MAX_ZOOM,
             x,
             y,
         });
-        surfaces.entry(surface.to_owned()).or_insert_with(|| Surface::new(surface));
+        let mut surface = surfaces.entry(surface_name.to_owned()).or_insert_with(|| Surface {
+            name: surface_name,
+            tiles: vec![],
+            min_x: x,
+            max_x: x,
+            min_y: y,
+            max_y: y,
+        });
+        surface.max_x = std::cmp::max(x, surface.max_x);
+        surface.min_x = std::cmp::min(x, surface.min_x);
+        surface.max_y = std::cmp::max(y, surface.max_y);
+        surface.min_y = std::cmp::min(y, surface.min_y);
 
-        /*
-        .load(Tile {
+        surface.tiles.push(Tile {
             zoom: MAX_ZOOM,
             x,
             y,
         });
-        */
     }
-    for surface in surfaces.values_mut() {
-        let s1 = surface.clone();
-        let s2 = surface.clone();
-        let s3 = surface.clone();
-        let s4 = surface.clone();
-        let q1 = thread::spawn(move || {
-            s1.load(Tile {
-                zoom: 15,
-                x: -1,
-                y: -1,
-            });
-        });
-        let q2 = thread::spawn(move || {
-            s2.load(Tile {
-                zoom: 15,
-                x: 0,
-                y: -1,
-            });
-        });
-        let q3 = thread::spawn(move || {
-            s3.load(Tile {
-                zoom: 15,
-                x: -1,
-                y: 0,
-            });
-        });
-        let q4 = thread::spawn(move || {
-            s4.load(Tile {
-                zoom: 15,
-                x: 0,
-                y: 0,
-            });
-        });
-        q1.join().expect("q1 panicked");
-        q2.join().expect("q2 panicked");
-        q3.join().expect("q3 panicked");
-        q4.join().expect("q4 panicked");
+    for (name, surface) in surfaces {
+        println!("{surface:#?}");
+        create_tiles(&surface);
+        //create_map(surface);
     }
     //println!("{:#?}", surfaces);
 }
